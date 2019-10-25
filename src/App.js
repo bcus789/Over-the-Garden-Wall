5d68056b905c03c0a107464463d038eadc878cf8
unknown <h.e.ergaster@gmail.com>
2019-10-25 15:34:07 -0700

    back to working order

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
-import NavBar from "./components/NavBar/navBar";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import "./App.css";
 
 class App extends Component {
   state = {
@@ -15,35 +11,6 @@ class App extends Component {
     topScore: 0,
     clicked: [],
     topMessage: "Click an image to begin"
-  };
-
-  removeFriend = id => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    this.setState({ friends });
-  };
-
-  clickedImage = props => {
-    if (this.state.clicked.includes(props.id) === false) {
-      this.state.clicked.push(props.id);
-      this.setState({
-        score: this.state.score + 1
-      });
-      if (this.state.score >= this.state.topScore) {
-        this.setState(prevState => ({
-          topScore: prevState.score,
-          topMessage: "You guessed correctly!"
-        }));
-      }
-    } else {
-      this.setState({
-        score: 0,
-        clicked: [],
-        topMessage: "You guessed incorrectly!"
-      });
-      if (this.state.score >= this.state.topScore) {
-        this.setState({ topScore: this.state.score });
-      }
-    }
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
