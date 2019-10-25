d8176a9224b3ae906a6ca9d11ce875921ab1f404
unknown <h.e.ergaster@gmail.com>
2019-10-25 15:45:40 -0700

    added scores

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
+import NavBar from "./components/NavBar/navBar";
 
 class App extends Component {
   state = {
     friends,
     score: 0,
     topScore: 0,
-    clicked: [],
-    topMessage: "Click an image to begin"
+    clicked: []
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <Wrapper>
-        <Title>Over the Garden Wall</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <NavBar scores={this.state} />
+        <Wrapper>
+          <Title>Over the Garden Wall</Title>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
