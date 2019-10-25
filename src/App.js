abef3e2f84a896bc26f0429469ca7740115b02b8
unknown <h.e.ergaster@gmail.com>
2019-10-25 12:36:41 -0700

    working template

--- src/App.js
+++ src/App.js
@@ -1,122 +1,23 @@
-import React, { Component } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Board from "./components/Board";
-import characters from "./img";
-import { shuffleArray, genArray } from "./utils/array";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
 
-class App extends Component {
-  state = {
-    topScore: 0,
-    score: 0,
-    characters,
-    order: shuffleArray(genArray(0, characters.length - 1)),
-    message: "Click on any picture to begin!",
-    won: false,
-  };
-
-  handleClick = propsCharName => {
-    const charIndex = this.state.characters.findIndex(
-      character => character.path === propsCharName
-    );
-
-    if (charIndex === -1)
-      return this.setMessage("Something went wrong! Try reloading the page.");
-
-    if (this.state.characters[charIndex].clicked === true) {
-      this.setMessage("Oops! Click on any picture to start over!");
-      this.resetScore();
-      this.unClickAllCharacters();
-    } else {
-      this.setCharacterToClicked(charIndex);
-      this.incrementScore();
-    }
-    this.shuffleCharacters();
-  };
-
-  newGame = () => {
-    this.unClickAllCharacters();
-    this.resetScore();
-    this.resetTopScore();
-    this.setMessage("Click on any picture to begin!");
-    this.setState({ won: false });
-  };
-
-  setMessage = message => {
-    this.setState({ message });
-  };
-
-  setCharacterToClicked = charIndex => {
-    this.setState(state => {
-      state.characters[charIndex].clicked = true;
-      return {
-        characters: state.characters,
-      };
-    });
-  };
-
-  unClickAllCharacters = () => {
-    this.setState(state => {
-      return {
-        characters: state.characters.map(character => {
-          character.clicked = false;
-          return character;
-        }),
-      };
-    });
-  };
-
-  incrementScore = () => {
-    this.setState(state => {
-      const won = state.score + 1 === state.characters.length;
-
-      return {
-        score: state.score + 1,
-        topScore:
-          state.topScore === state.score ? state.topScore + 1 : state.topScore,
-        won: won ? true : false,
-        message: won
-          ? "Congratulations! You've won!"
-          : "Don't click on the same picture again!",
-      };
-    });
-  };
-
-  resetScore = () => {
-    this.setState({ score: 0 });
-  };
-
-  resetTopScore = () => {
-    this.setState({ topScore: 0 });
-  };
-
-  shuffleCharacters = () => {
-    this.setState(state => {
-      return { order: shuffleArray(state.order) };
-    });
-  };
-
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-        <Navbar
-          topScore={this.state.topScore}
-          score={this.state.score}
-          message={this.state.message}
-        />
-        <Board
-          handleClick={this.handleClick}
-          topScore={this.state.topScore}
-          score={this.state.score}
-          order={this.state.order}
-          characters={this.state.characters}
-          won={this.state.won}
-          newGame={this.newGame}
-        />
-        <Footer />
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
